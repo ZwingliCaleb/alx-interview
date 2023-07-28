21cac3ceb561ad64515ed1d5161af6922527e1c3
Caleb Zwingli <84632961+ZwingliCaleb@users.noreply.github.com>
2023-07-28 13:06:12 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,16 @@
+#!/usr/bin/python3
+"""
+Print Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
-    """
-    Generate Pascal's triangle of size n.
-
-    Args:
-        n (int): The size of Pascal's triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's triangle.
-              Returns an empty list if n <= 0.
-    """
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)
-        triangle.append(new_row)
-
-    return triangle
+    """returns Pascal's triangle in a list of list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][a] + my_list[i][a + 1]
+                              for a in range(len(my_list[i]) - 1)] + [1])
+    return my_list
