a529160d9f9691174b54d84a9c5b1f7e54cc3378
Caleb Zwingli <84632961+ZwingliCaleb@users.noreply.github.com>
2023-08-11 12:10:58 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
+
+""" Minimum Operations """
+
+
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to get 'n' H characters in a text file.
-
-    :param n: The desired number of H characters.
-    :return: The minimum number of operations required.
-             Returns 0 if achieving 'n' is impossible.
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+    Returns an integer
+    If n is impossible to achieve, returns 0
     """
-
-    if n <= 0:
+    if not isinstance(n, int):
         return 0
 
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        dp[i] = float('inf')  # Initialize with a large value
-        for j in range(1, int(i ** 0.5) + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j - 1)
-                dp[i] = min(dp[i], dp[i // j] + j - 1)
-
-    return dp[n]
-
-# Example usage
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    operations = 0
+    iterator = 2
+    while (iterator <= n):
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations
