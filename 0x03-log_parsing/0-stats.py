85fbb86d1baed885b630205d068a43465d9b5c4d
Caleb Zwingli <84632961+ZwingliCaleb@users.noreply.github.com>
2023-08-17 22:48:31 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
 
+""" This is a script that reads stdin line by line and computes metrics """
+
 import sys
 
-def print_metrics(status_codes, total_size):
-    print("Total file size:", total_size)
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] != 0:
-            print("{}: {}".format(code, status_codes[code]))
 
-def parse_line(line):
-    try:
-        parts = line.split()
-        ip_address = parts[0]
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-        return ip_address, status_code, file_size
-    except Exception:
-        return None, None, None
+def printStatus(dic, size):
+    """ starts Printing """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-total_size = 0
-line_count = 0
+
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        if line_count != 0 and line_count % 10 == 0:
-            print_metrics(status_codes, total_size)
-        
-        ip_address, status_code, file_size = parse_line(line)
-        
-        if ip_address is not None and status_code in status_codes:
-            total_size += file_size
-            status_codes[status_code] += 1
-        
-        line_count += 1
-    
-    print_metrics(status_codes, total_size)
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
+
 
 except KeyboardInterrupt:
-    print_metrics(status_codes, total_size)
+    printStatus(statusCodes, size)
     raise
-
